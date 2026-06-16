aa973f3755b6728ad696bbff51113152f3efc69e
Rafaela Moraes Moreira Da Silva <125367369+Rafaela-Moraes@users.noreply.github.com>
2026-06-16 18:13:17 -0300

    MODEL/
    MODEL ATUALIZADO 16/06

--- MODEL/cliente.php
+++ MODEL/cliente.php
@@ -4,6 +4,7 @@ namespace MODEL;
 class Cliente {
     private ?int $id ;
     private ?string $nome;
+    private ?string $cpf;
     private ?string $email;
     private ?string $telefone;
     private ?string $cidade;
@@ -23,6 +24,12 @@ class Cliente {
     }
     public function setNome(string $nome) { 
         $this->nome = $nome; 
+    }
+    public function getCpf() { 
+        return $this->cpf; 
+    }
+    public function setCpf(string $cpf) { 
+        $this->cpf = $cpf; 
     }
 
     public function getEmail() { 
@@ -52,6 +59,24 @@ class Cliente {
     public function setEstado(string $estado) { 
         $this->estado = $estado; 
     }
+
+    public function validarCpf($cpf) {
+    $cpf = preg_replace('/[^0-9]/', '', $cpf);
+     
+    if (strlen($cpf) != 11 || preg_match('/(\d)\1{10}/', $cpf)) {
+        return false;
+    }
+    for ($t = 9; $t < 11; $t++) {
+        for ($d = 0, $c = 0; $c < $t; $c++) {
+            $d += $cpf[$c] * (($t + 1) - $c);
+        }
+        $d = ((10 * $d) % 11) % 10;
+        if ($cpf[$c] != $d) {
+            return false;
+        }
+    }
+    return true;
+}
 
 }
 ?>
