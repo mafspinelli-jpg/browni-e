1f392f9e5fcb2056e5ac77e4d9905b9b33e03d5f
Rafaela Moraes Moreira Da Silva <125367369+Rafaela-Moraes@users.noreply.github.com>
2026-06-16 18:14:38 -0300

    Add files via upload
    Atualizado 16/06

--- login.php
+++ login.php
@@ -25,22 +25,41 @@ if (isset($_POST['entrar'])) {
 <head>
     <meta charset="UTF-8">
     <title>Login - Browni-e</title>
+    <link rel="stylesheet" href="CSS/estilo.css">
 </head>
+
 <body>
+    <div  class="acesso">
+            <div class="menu_acesso">
+                <img class="acesso_Logotipo" src="/brownie/IMG/Logotipo.png" alt="Browni-e">
+                <img class="acesso_Logo" src="/brownie/IMG/Logo.png" alt="Browni-e">
+                <p class="">Sistema de Gestão de Estoque</p>
+                <ul>
+                    <li>Controle de ingredientes em tempo real</li>
+                    <li>Relatórios e gráficos detalhados</li>
+                    <li>Gestão de categorias e produtos</li>
+                    <li>Alertas de estoque mínimo</li>
+                </ul>
+            </div>
+                
 
-    <h2>Acesso ao Sistema Browni-e</h2>
+            <div class="acesso_form">
+                <form method="POST">
+                <h1 class="acesso_title">Bem-vindo de volta</h1>
+                <p class="acesso_title">Entre com suas credenciais para acessar o sistema.</p>
 
-    <?php if (isset($erro)) { echo "<p style='color: red;'>$erro</p>"; } ?>
+                <?php if (isset($erro)) { echo "<p class='error-message'>$erro</p>"; } ?>
 
-    <form method="POST">
-        <label>E-mail:</label><br>
-        <input type="email" name="email" required><br><br>
+                
+                        <label for="email">E-mail</label>
+                        <input type="email" id="email" name="email" placeholder="seu@email.com" required>
+        
+                        <label for="senha">Senha</label>
+                        <input type="password" id="senha" name="senha" placeholder="••••••••" required>
+                    <button type="submit" name="entrar" class="btn-submit">Entrar</button>
 
-        <label>Senha:</label><br>
-        <input type="password" name="senha" required><br><br>
-
-        <button name="entrar">Entrar no Sistema</button>
-    </form>
-
+                </form>
+            </div>
+</div>
 </body>
 </html>
